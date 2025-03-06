538b9e9e8d3269e2006952d472d9719514b4ac7f
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-03-06 12:30:52 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// upperCaseCustomerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// setBestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// overwriteBestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// changeLeastFavoriteCustomer
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob'; // This will throw an error
+}
